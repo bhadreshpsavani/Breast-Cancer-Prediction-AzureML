02b44924b51e00e459915d449906905a7eff72ba
Bhadresh Savani <bhadreshpsavani@gmail.com>
2021-02-06 07:18:40 +0000

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,14 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
 import argparse
 import os
 import numpy as np
 from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from sklearn import datasets
+from sklearn.metrics import f1_score
 
 run = Run.get_context()
 
@@ -16,25 +16,31 @@ def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-
+    parser.add_argument('--n_estimators', type=int, default=10, help="The number of boosting stages to perform. Gradient boosting is fairly robust to over-fitting so a large number usually results in better performancThe maximum depth of the individual regression estimators. The maximum depth limits the number of nodes in the tree.")
+    parser.add_argument('--learning_rate', type=float, default=0.1, help="Learning rate shrinks the contribution of each tree by learning_rateThe number of boosting stages to perform. Gradient boosting is fairly robust to over-fitting so a large number usually results in better performancThe maximum depth of the individual regression estimators. The maximum depth limits the number of nodes in the tree.")
+    parser.add_argument('--max_depth', type=int, default=1, help="The maximum depth of the individual regression estimators. The maximum depth limits the number of nodes in the tree.")
+    
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
+    run.log("Number of estimators:", np.int(args.n_estimators))
+    run.log("Learning Rate:", np.float(args.learning_rate))
+    run.log("Maximum Depth of Tree:", np.int(args.max_depth))
 
     data = datasets.load_breast_cancer()
 
     x, y = data.data, data.target
 
-    x_train, x_test, y_train, y_test=train_test_split(x, y)
+    x_train, x_test, y_train, y_test=train_test_split(x, y, random_state=12345)
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    model = GradientBoostingClassifier(
+        n_estimators=args.n_estimators, 
+        learning_rate=args.learning_rate, 
+        max_depth=args.max_depth, 
+        random_state=12345).fit(x_train, y_train)
 
-    accuracy = model.score(x_test, y_test)
-
-    run.log("Accuracy", np.float(accuracy))
+    y_pred = model.predict(x_test)
+    f1 = f1_score(y_test, y_pred, average='macro')
+    run.log("F1 Score", np.float(f1))
 
     os.makedirs('outputs', exist_ok=True)
     joblib.dump(model, 'outputs/model.joblib')
